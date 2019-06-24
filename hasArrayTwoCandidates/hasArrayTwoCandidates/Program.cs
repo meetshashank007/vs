99d86a9b65959db7d8aebac26e2a6a0b4f00bfcd
meetshashank007 <meet.shashank007@gmail.com>
2019-06-25 02:12:08 +0530

    Given an array A[] and a number x, check for pair in A[] with sum as x

---
+++ hasArrayTwoCandidates/hasArrayTwoCandidates/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+/*
+ * Write a program that, given an array A[] of n numbers and another number x,
+ * determines whether or not there exist two elements in S whose sum is exactly x.
+ */
+namespace hasArrayTwoCandidates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> arrList = new List<int>();
+            var inputList = string.Empty;
+            Console.WriteLine("Please enter the length of input array:");
+            var countValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter your for test: ");
+            inputList = "{ ";
+            for (int i = 0; i < countValue; i++)
+            {
+                int x = Convert.ToInt32(Console.ReadLine());
+                arrList.Add(x);
+                inputList += x.ToString();
+                if(i < countValue-1)
+                {
+                    inputList += ", ";
+                }
+                else
+                {
+                    inputList += " }";
+                }
+            }
+            Console.WriteLine("Your input array => ");
+            Console.WriteLine(inputList);
+            Console.WriteLine("Please enter the sum to chek in the list for the pair:");
+            var sum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your input to check => ");
+            Console.WriteLine(sum);
+            hasArrayTwoCandidates(arrList.ToArray(), sum);
+            Console.ReadKey();
+        }
+        public static bool hasArrayTwoCandidates(int[] arr, int sum)
+        {
+            int l = 0, r, arrLength;
+            r = arrLength = arr.Length - 1;
+            var result = from element in arr
+                         orderby element ascending
+                         select element;
+            arr = result.ToArray();
+            while (arr[l] < arr[r])
+            {
+                if(arr[l] + arr[r] == sum)
+                {
+                    Console.WriteLine("Pair found : {0} + {1} = {2}", arr[l], arr[r], sum);
+                    return true;
+                }
+                else if(arr[l] + arr[r] < sum)
+                {
+                    l++;
+                }
+                else if(arr[l] + arr[r] > sum)
+                {
+                    r--;
+                }
+            }
+            return false;
+        }
+    }
+}
